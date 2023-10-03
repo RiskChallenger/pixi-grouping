c1b10d10803eb26816404abc2a539279b61517df
Rien Heuver <rienheuver@gmail.com>
2023-10-03 15:16:45 +0200

    fix: export styleservice

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,4 @@ export * from "./classes/Block";
 export * from "./classes/DragContainer";
 export * from "./classes/Group";
 export * from "./classes/GroupingApplication";
+export * from "./classes/StyleService";
